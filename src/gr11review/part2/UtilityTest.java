12c42f5509fef9364f5d74b6f476e60a650b2383
matthewyam <matthew.yam23@ycdsbk12.ca>
2022-09-28 21:01:45 -0400

    updates

--- src/gr11review/part2/UtilityTest.java
+++ src/gr11review/part2/UtilityTest.java
@@ -1,10 +1,30 @@
 package gr11review.part2;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
 
 import gr11review.part2.Utility;
 
 public class UtilityTest{
+    @Test
+    void xyBalanceTest1(){
+        assertEquals(true, Utility.xyBalance("aaxbby"));
+    }
+
+    @Test
+    void longestWordTest1() throws IOException{
+        assertEquals("conservation", Utility.longestWord("C:\\Users\\mykay\\github-classroom\\SACHSTech\\grade-11-review-2---methods-and-testing-matthew-parker\\src\\gr11review\\test2\\Review2_3Test_1.txt"));
+    }
+
+
+    /**
+    @Test
+    void withoutTenTest1(){
+        assertEquals("1, 2, 0, 0", Utility.withoutTen());
+    }
+    */
   
 }
