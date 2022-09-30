c8d3c69ef6ba62e7b88301e95081e0aa0eb02e7f
matthewyam <matthew.yam23@ycdsbk12.ca>
2022-09-29 23:02:26 -0400

    done 3 and started 4

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -10,15 +10,24 @@ import java.util.ArrayList;
 
 public class Utility {
 
+    /**
+     * @param str reads the string from test file
+     * @return returns the string
+     */
+
     public static boolean xyBalance(String str){
         
         boolean why = false;
 
+        //checks if there is the letter "y" in the string
         for(int i = str.length()-1; i >= 0; i--){
+            
+            //determines that the letter "y" is in the string
             if(str.charAt(i) == 'y'){
                 why = true;
             }
-
+            
+            //returns false statement if the parameters are not met
             if(str.charAt(i) == 'x' && why == false){
                 return false;
             }
@@ -28,64 +37,90 @@ public class Utility {
         
     }
 
+    /**
+     * @param filenametxt reads file
+     * @return returns the longest word
+     * @throws IOException gives error for input or output fails
+     */
     public static String longestWord(String filenametxt) throws IOException{
+
+        //reads file
         BufferedReader text = new BufferedReader(new FileReader(filenametxt));
 
+        //sets the longest word to null and reads the first line
         String longWord = " ";
         String line = text.readLine();
 
-
+        //reads file line by line until it hits a blank one
         while(line != null){
 
+            //sets the longest word if current word is longer than the one set
             if(line.length() > longWord.length()){
 
                 longWord = line;
             }
-            
+            //reads next line of text
             line = text.readLine();
         }
 
+        //closes text file
         text.close();
         
+        //returns the longest word
         return longWord;
     }
-
-
+/**
+ * @param nums pull array
+ * @return returns the copied array
+ */
     public static int[] withoutTen(int[] nums){
 
-        for(int count = 0; count < nums.length-1; count++){
+        //creates new array to copy to and set the counter for it to 0
+        int[] numsTenless = new int[nums.length];
+        int numTenlessCounter = 0;
 
-            if(nums[count] == 10){
-                nums[count] = nums[count+1];
-                nums[nums.length-1] = 0;
+        //count the numbers in the array
+        for(int count = 0; count < nums.length; count++){
 
+            //copies numbers that are not equal to 10 to the new array
+            if(nums[count] != 10){
+
+                numsTenless[numTenlessCounter] = nums[count];
+                numTenlessCounter++;
             }
 
+            //add zeros to the end of the array to fill in the missing numbers
+            if(numTenlessCounter < nums.length){
+                numsTenless[numTenlessCounter] = 0;
+            }
         }
-
-        return nums;
+        return numsTenless;
     }
     
     public static boolean linearIn(int[] outer, int[] inner){
 
         int counter = 0;
+        boolean dupe = false;
 
         for (int outerCount = 0; outerCount < outer.length-1; outerCount++){
             for (int innerCount = 0; innerCount < inner.length-1; innerCount++){
 
                 if(outer[outerCount] == inner[innerCount]){
-                    counter++;
+                    dupe = true;
                 }
-
             }
 
+            if(dupe = true){
+                counter++;
+                dupe = false;
+            }
         }
 
         if(counter == inner.length-1){
             return true;
         }
-
-
         return false;
     }
 }
+
+
--- src/gr11review/part2/UtilityTest.java
+++ src/gr11review/part2/UtilityTest.java
@@ -11,21 +11,35 @@ import org.junit.Assert;
 import gr11review.part2.Utility;
 
 public class UtilityTest{
+
+    //Arrays for withoutTenTest
+    int[] nums = {10, 2, 10};
+    int[] numAnswer = {2, 0, 0};
+    
+    int[] outer = {1, 2, 4, 6};
+    int[] inner = {2, 4};
+
+    //Test for xyBalance
     @Test
     void xyBalanceTest1(){
         assertEquals(true, Utility.xyBalance("aaxbby"));
     }
 
+    //Test fot longestWord
     @Test
     void longestWordTest1() throws IOException{
         assertEquals("conservation", Utility.longestWord("C:\\Users\\mykay\\github-classroom\\SACHSTech\\grade-11-review-2---methods-and-testing-matthew-parker\\src\\gr11review\\test2\\Review2_3Test_1.txt"));
     }
 
-
-    
+    //Test for withoutTen
     @Test
     void withoutTenTest1(){
-     
+        assertArrayEquals(numAnswer, Utility.withoutTen(nums));  
+    }
+
+    @Test
+    void linearInTest1(){
+        assertEquals(true, Utility.linearIn(outer, inner));
     }
     
   
